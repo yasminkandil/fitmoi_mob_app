fea8a8b1062f2e62b8b1f717aa04dce37a41a39b
salmawafaaa <salma1905060@miuegypt.edu.eg>
2023-02-05 17:25:16 +0200

    tryyy

--- lib/admin/add_category.dart
+++ lib/admin/add_category.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:fitmoi_mob_app/admin/add_product.dart';
